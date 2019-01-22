305a6f2b2528f85f16d3e4eeb85f30f88f94013d
Yassine <benyoussifyassine@gmail.com>
2019-01-22 18:53:43 +0100

    Last version, TP1 Rest service

--- src/main/java/co/simplon/smillet/rest/tp1/domain/model/utilisateur/IUtilisateurService.java
+++ src/main/java/co/simplon/smillet/rest/tp1/domain/model/utilisateur/IUtilisateurService.java
@@ -1,14 +1,17 @@
 package co.simplon.smillet.rest.tp1.domain.model.utilisateur;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IUtilisateurService {
-	
+
 	Utilisateur addUser(Utilisateur utilisateur);
+
 	Utilisateur getUser(long id);
+
 	List<Utilisateur> getUsers();
+
 	void deleteUser(long id);
+
 	void updateUser(Utilisateur utilisateur);
-	
+
 }
